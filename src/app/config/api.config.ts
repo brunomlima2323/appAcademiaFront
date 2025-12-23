a4690caf9d62e5604f4f8d81ff41d8808aee475c
Bruno Lima <bruno.lima@lynxspa.com>
2025-12-23 17:34:39 -0300

    melhorias estilo tela inicial

--- src/app/config/api.config.ts
+++ src/app/config/api.config.ts
@@ -1,4 +1,4 @@
 export const API_CONFIG = {
-  baseUrl: 'https://appacademiaback.onrender.com'
-  // baseUrl: 'http://localhost:8080'
+  // baseUrl: 'https://appacademiaback.onrender.com'
+  baseUrl: 'http://localhost:8080'
 }

36938631898d65e6f97271aafd0721166a1c89ba
Bruno Lima <bruno.lima@lynxspa.com>
2026-01-07 16:25:06 -0300

    alteracao config

--- src/app/config/api.config.ts
+++ src/app/config/api.config.ts
@@ -1,4 +1,5 @@
 export const API_CONFIG = {
-  baseUrl: 'https://appacademiaback.onrender.com'
+  baseUrl: 'https://appacademia-production.up.railway.app'
+  // baseUrl: 'https://appacademiaback.onrender.com'
   // baseUrl: 'http://localhost:8080'
 }
